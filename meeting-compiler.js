53d30e4739aa190266589d46327d5c38bc9f3603
Yosef Herskovitz <yosefherskovitz@gmail.com>
2020-09-11 12:24:23 -0400

    added catch for meetings that/end at the same time

--- meeting-compiler.js
+++ meeting-compiler.js
@@ -1,7 +1,7 @@
 // Given an array of meeting times, return an array of optomized and combined meetings.
 
 let array = [
-  { startTime: 0,  endTime: 1 }, 
+  { startTime: 0,  endTime: 1 },
   { startTime: 3,  endTime: 5 },
   { startTime: 4,  endTime: 8 },
   { startTime: 10, endTime: 12 },
@@ -13,8 +13,9 @@ let meetingApp = (array) => {
   array.forEach(meeting => {
     if(ans.length === 0) return ans[0] = meeting
     ans.forEach(finalMeeting => {
-      if(meeting.startTime < finalMeeting.endTime && meeting.endTime > finalMeeting.endTime) return finalMeeting.endTime = meeting.endTime
-      if(meeting.endTime < finalMeeting.endTime && meeting.startTime < finalMeeting.startTime) return finalMeeting.startTime = meeting.startTime
+      if(meeting.startTime === finalMeeting.endTime) finalMeeting.endTime = meeting.endTime
+      if(meeting.startTime < finalMeeting.endTime && meeting.endTime > finalMeeting.endTime) finalMeeting.endTime = meeting.endTime
+      if(meeting.endTime < finalMeeting.endTime && meeting.startTime < finalMeeting.startTime) finalMeeting.startTime = meeting.startTime
       else {return}
     })
     if (!ans.map(x => x.endTime).includes(meeting.endTime) && !ans.map(x => x.startTime).includes(meeting.startTime)) ans[ans.length + 1] = meeting
