d087d715cb57807e35a5d82d66be8037f84455f9
Yosef Herskovitz <yosefherskovitz@gmail.com>
2020-10-02 15:49:44 -0400

    ic-inflight-entertainment solution2

--- ic-inflight-entertainment.js
+++ ic-inflight-entertainment.js
@@ -1,25 +1,41 @@
 // Given a flight time of x, see if 2 movies form movieArray will equal the exact flight time.
 
-const flightTime = 240 // minutes
-const movieTimes = [100, 125, 90, 85, 115] // true
+const flightTime = 240; // minutes
+const movieTimes = [100, 125, 90, 85, 115]; // true
 // const movieTimes = [100, 125, 90, 85, 110] // false
 
 // ans 1
 // time complexity: O(n log n)
 // space complexity: (1)
-const flightMovieChecker = (flightTime, movies) => {
-  let ans = false
-  let inner = (flightTime, movies, counter = 0) => {
-    while(counter < movies.length) {
-      for (let i = counter + 1; i < movies.length; i++) {
-        if (movies[i] + movies[counter] === flightTime) {ans = true;
-        break}
-      }
-      counter++
-  }
-}
-  inner(flightTime, movies)
-  return ans
-}
+// const flightMovieChecker = (flightTime, movies) => {
+//   let ans = false;
+//   let inner = (flightTime, movies, counter = 0) => {
+//     while (counter < movies.length) {
+//       for (let i = counter + 1; i < movies.length; i++) {
+//         if (movies[i] + movies[counter] === flightTime) {
+//           ans = true;
+//           break;
+//         }
+//       }
+//       counter++;
+//     }
+//   };
+//   inner(flightTime, movies);
+//   return ans;
+// };
 
-console.log(flightMovieChecker(flightTime, movieTimes))
+// ans2
+// time complexity: O(n)
+const flightMovieChecker = (flightTime, movies) => {
+  const checkedMovies = new Set();
+
+  for (let i = 0; i < movies.length; i++) {
+    const matchingMovie = flightTime - movies[i]
+
+    if (checkedMovies.has(matchingMovie)) return true
+    checkedMovies.add(movies[i])
+  }
+  return false
+};
+
+console.log(flightMovieChecker(flightTime, movieTimes));
