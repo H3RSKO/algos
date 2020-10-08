d38a9bbfa05f0ad8516e8271b0679960cad5be50
Yosef Herskovitz <yosefherskovitz@gmail.com>
2020-10-08 10:37:00 -0400

    added second ans for any permutation palindrome

--- ic-any-permutation-palindrome.js
+++ ic-any-permutation-palindrome.js
@@ -7,16 +7,28 @@ const string = 'civic' // true
 
 // Ans 1
 // Time Complexity: 0(n)
+// const possiblePalindromeChecker = (string) => {
+//   const ans = {}
+//   string.split('').forEach(x => {
+//     if(Object.keys(ans).includes(x)) ans[x] += 1
+//     else {ans[x] = 1}
+//   })
+//   // if more than one value is odd return false
+//   const finalAns = Array.from(Object.values(ans)).filter(x => x % 2)
+//   if (finalAns.length > 1) return false
+//   return true
+// }
+
+// Ans 2
+// Time complexity: 0(n)
 const possiblePalindromeChecker = (string) => {
-  const ans = {}
-  string.split('').forEach(x => {
-    if(Object.keys(ans).includes(x)) ans[x] += 1
-    else {ans[x] = 1}
-  })
-  // if more than one value is odd return false
-  const finalAns = Array.from(Object.values(ans)).filter(x => x % 2)
-  if (finalAns.length > 1) return false
-  return true
+  const unpairedLetters = new Set()
+  for (let char of string) {
+    if(unpairedLetters.has(char)) unpairedLetters.delete(char)
+    else unpairedLetters.add(char)
+  }
+  return unpairedLetters.size <= 1
 }
+
 
 console.log(possiblePalindromeChecker(string))
