18c89d96077bc97bf16b3a9cb9f069ac2039aab8
Yosef Herskovitz <yosefherskovitz@gmail.com>
2020-11-18 11:21:01 -0500

    ic-in-place-shuffle ans 2

--- ic-in-place-shuffle.js
+++ ic-in-place-shuffle.js
@@ -3,12 +3,25 @@
 let array = [1, 2, 3, 4, 5]
 
 // Ans 1: Time Complexity: 0(n^n)
+// const inPlaceShuffle = (array) => {
+//   for (let i = 0; i < array.length; i++) {
+//     let randomNum = getRandom(0, array.length)
+//     let currentNum = array[i]
+//     array.splice(i, 1, array[randomNum]) // 0(n)
+//     array.splice(randomNum, 1, currentNum) // 0(n)
+//   }
+// }
+
+// Ans 2: Time Complexity: 0(n)
+// Uses Fisher-Yates shuffle
 const inPlaceShuffle = (array) => {
-  for (let i = 0; i < array.length; i++) {
-    let randomNum = getRandom(0, array.length)
-    let currentNum = array[i]
-    array.splice(i, 1, array[randomNum])
-    array.splice(randomNum, 1, currentNum)
+  let length = array.length - 1
+  while (length >= 0) {
+    let currentNum = array[length]
+    let randomNum = getRandom(0, length)
+    array[length] = array[randomNum]
+    array[randomNum] = currentNum
+    length--
   }
 }
 
